5d49e3827d2cfadc03c7c40cc2c4fa0b598beb37
wklchris <wklchris@hotmail.com>
2023-03-29 15:20:25 -0700

    Rubik: Fix jquery failure & roofpig CSS.

--- docsrc/Rubik/conf.py
+++ docsrc/Rubik/conf.py
@@ -3,7 +3,7 @@ copyright = "wklchris"
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 extensions = ['nbsphinx', 'sphinx_copybutton', 'sphinx.ext.mathjax', 'sphinx.ext.extlinks']
 html_css_files = ['style.css', 'roofpig.css']
-html_js_files = ['roofpig_and_three.min.js']
+html_js_files = ['jquery.js', 'roofpig_and_three.min.js']
 html_static_path = ['_static', '../_static']
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {'canonical_url': 'https://wklchris.github.io/Rubik/'}
@@ -22,8 +22,8 @@ rst_prolog = """
 # sphinx.ext.extlinks
 extlinks = {
    'algdb': ("http://algdb.net/puzzle/333/%s", 'AlgDB: %s'),
-   'algdb-pll': ("http://algdb.net/puzzle/333/pll/%s", 'AlgDB: PLL '),
-   'algdb-oll': ("http://algdb.net/puzzle/333/oll/%s", 'AlgDB: OLL '),
-   'speedsolving': ("https://www.speedsolving.com/wiki/index.php/%s", 'Speedsolving Wiki: '),
-   'jperm': ("https://jperm.net/algs/%s", 'Jperm.net: ')
+   'algdb-pll': ("http://algdb.net/puzzle/333/pll/%s", 'AlgDB: PLL %s'),
+   'algdb-oll': ("http://algdb.net/puzzle/333/oll/%s", 'AlgDB: OLL %s'),
+   'speedsolving': ("https://www.speedsolving.com/wiki/index.php/%s", 'Speedsolving Wiki: %s'),
+   'jperm': ("https://jperm.net/algs/%s", 'Jperm.net: %s')
 }
